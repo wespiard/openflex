82ca997a23dbdec3f64c0f71d1a23d7889cc1e42
Wesley Piard <1278350+wespiard@users.noreply.github.com>
2024-03-11 09:33:10 -0400

    feat(main): add `Config` class definition (mostly empty)

--- rex/__main__.py
+++ rex/__main__.py
@@ -1,5 +1,16 @@
+import yaml
+
+
+class Config:
+    """Contains the configuration data supplied by a YAML file."""
+
+    def __init__(self, config_file) -> None:
+        config = yaml.load(config_file, Loader=yaml.loader.BaseLoader)
+
+
 def main():
-    print(f"Hello")
+    print("Hello")
+
 
 if __name__ == "__main__":
     main()
